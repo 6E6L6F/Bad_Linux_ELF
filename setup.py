78bfda85117c510d71ca596cd80a843180713e05
ELF <87319528+6E6L6F@users.noreply.github.com>
2023-07-31 14:08:36 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='ELF_Linux',
     packages=find_packages(include=['ELF_Linux']),
-    version='0.1.0',
+    version='1.2.0',
     description='The This Lab For The Create A Malware',
     author='ELF',
     license='MIT',
